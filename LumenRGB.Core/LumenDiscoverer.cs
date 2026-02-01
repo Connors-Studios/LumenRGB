36afb8598fbbbeb3ad36288f51d087a7b7207bf2
Connortp0 <106664737+Connortp0@users.noreply.github.com>
2026-02-01 19:04:59 +0100

    Added Discovering of Lumen Devices.

---
+++ LumenRGB.Core/LumenDiscoverer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumenRGB.Core
+{
+    public class LumenDiscoverer
+    {
+        // These values are used to identify Lumen devices
+        string LUMEN_VID = "303A"; // Espressif VID (NOT REGISTERED and WILL BE CHANGED)
+        string LUMEN_PID = "4002"; // Espressif PID (NOT REGISTERED and WILL BE CHANGED)
+        
+        public void DiscoverLumenDevices()
+        {
+            var devices = GetDevices();
+            var lumenDevices = devices
+                .Where(d => string.Equals(d.Pid, LUMEN_PID, StringComparison.OrdinalIgnoreCase)
+                            && string.Equals(d.Vid, LUMEN_VID, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
+
+        public List<UsbSerialDevice> GetDevices()
+        {
+            var devices = UsbSerialEnumerator.GetComDevices();
+            return devices;
+        }
+    }
+}
